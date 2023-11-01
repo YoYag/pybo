2efca3f5ecd36d1f30c50f9a613020b57bad9d7d
YoYag <la2043432@gmail.com>
2023-11-01 15:46:23 +0900

    템플릿을 이용한 질문목록 출력

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 
-def index(repuest) :
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+def index(request) :
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
