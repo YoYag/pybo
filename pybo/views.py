badd45de9af1dd1a1e3fdd325c862bae8ef438c3
YoYag <la2043432@gmail.com>
2023-11-02 10:23:11 +0900

    답변 등록 - 2

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Question
+from .models import Question, Answer
 
 def index(request) :
     question_list = Question.objects.order_by('-create_date')
@@ -14,5 +14,6 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
     return redirect('pybo:detail', question_id=question.id)
